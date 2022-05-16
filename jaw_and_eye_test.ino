6e5227369a822477ac08924ea9b59fd814546661
eskitete <65063591+eskitete@users.noreply.github.com>
2022-05-16 12:36:05 -0500

    Add files via upload

--- jaw_and_eye_test.ino
+++ jaw_and_eye_test.ino
@@ -1,38 +1,23 @@
-  // Include the Servo library
 #include <Servo.h>
 // Declare the Servo pin
 int servoPin = 9;
 int servoPin2 = 10;
 // Create a servo object
+Servo Servo2;
 Servo Servo1;
-Servo Servol2;
 void setup() {
-  // We need to attach the servo to the used pin number
+  Servo2.attach(servoPin2);
   Servo1.attach(servoPin);
-  Servol2.attach(servoPin2);
 }
 void loop() {
-
+   Servo1.write(180);
+  delay(2000);
+  Servo2.write(90);
+  delay(2000);
   Servo1.write(0);
+   Servo2.write(180);
+  delay(2000);
+   Servo2.write(0);
   delay(2000);
   
-  Servol2.write(0);
-  delay(2000);
-
-  Servo1.write(90);
-  delay(1000);
-  // Make servo go to 180 degrees
- 
-  
-  Servol2.write(170);
-  delay(2000);
-    
-  Servo1.write(170);
-  delay(2000);  
-   
-
-
-  // Make servo go to 180 degrees
-
-
 }
